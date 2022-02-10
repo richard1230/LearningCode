fbff07440a9526d2103b76cd27d5bba101ad9b97
richard1230 <yongjinhu1230@gmail.com>
2022-02-10 23:10:11 +0800

    查找资料

---
+++ Javascript/Js算法和数据结构/资料查找.js
@@ -0,0 +1,44 @@
+// 设置
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  // 只修改这一行下面的代码
+
+
+  for (var i = 0; i < contacts.length; i++) {
+    if(contacts[i].firstName === name) {
+      return contacts[i][prop] || "No such property";
+    }
+  }
+  return "No such contact";
+
+
+  // 只修改这一行上面的代码
+}
+
+lookUpProfile("Akira", "likes");
