eb6aa0d38e8f2f4bf4ebd6cd00e56639bc6add8a
richard1230 <yongjinhu1230@gmail.com>
2022-02-10 22:00:03 +0800

    记录集合-1

--- Javascript/Js算法和数据结构/记录集合.js
+++ Javascript/Js算法和数据结构/记录集合.js
@@ -66,4 +66,15 @@ function updateRecords(records, id, prop, value) {
 
 updateRecords(recordCollection, 5439, 'artist', 'ABBA');
 
+updateRecords(recordCollection, 5439, "tracks", "Take a Chance on Me")
+
+updateRecords(recordCollection, 2548, "artist", "")
+
+updateRecords(recordCollection, 1245, "tracks", "Addicted to Love")
+
+updateRecords(recordCollection, 2468, "tracks", "Free")
+
+updateRecords(recordCollection, 2548, "tracks", "")
+
+updateRecords(recordCollection, 1245, "albumTitle", "Riptide")
 
