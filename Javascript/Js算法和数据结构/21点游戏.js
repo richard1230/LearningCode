3d34878be69eec8de55dfc3dd78bd2dae004c5ad
richard1230 <yongjinhu1230@gmail.com>
2022-02-10 12:00:31 +0800

    21点游戏

---
+++ Javascript/Js算法和数据结构/21点游戏.js
@@ -0,0 +1,35 @@
+let count = 0;
+
+function cc(card) {
+  // 只修改这一行下面的代码
+  switch(card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+
+  var holdbet = 'Hold'
+  if (count > 0) {
+    holdbet = 'Bet'
+  }
+
+  return count + " " + holdbet;
+  // 只修改这一行上面的代码
+}
+
+console.log(cc(2));
+console.log(cc(3));
+console.log(cc(7));
+console.log(cc('K'));
+console.log(cc('A'));
