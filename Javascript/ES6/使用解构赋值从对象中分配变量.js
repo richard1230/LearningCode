f06c24c28f57f393041836ef851c00a285c19451
richard1230 <yongjinhu1230@gmail.com>
2022-02-16 21:15:31 +0800

    使用解构赋值从对象中分配变量

---
+++ Javascript/ES6/使用解构赋值从对象中分配变量.js
@@ -0,0 +1,13 @@
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// 只修改这一行下面的代码
+
+// const highToday = HIGH_TEMPERATURES.today;
+// const highTomorrow = HIGH_TEMPERATURES.tomorrow;
+
+const {today: highToday, tomorrow: highTomorrow} = HIGH_TEMPERATURES
+// 只修改这一行上面的代码
