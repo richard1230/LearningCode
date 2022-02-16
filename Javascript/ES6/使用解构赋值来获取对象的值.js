44aa62cbf00370b1beb1082cc5a90d7e886baac1
richard1230 <yongjinhu1230@gmail.com>
2022-02-16 21:10:07 +0800

    使用解构赋值来获取对象的值

---
+++ Javascript/ES6/使用解构赋值来获取对象的值.js
@@ -0,0 +1,12 @@
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// 只修改这一行下面的代码
+
+// const today = HIGH_TEMPERATURES.today;
+// const tomorrow = HIGH_TEMPERATURES.tomorrow;
+const {today, tomorrow} = HIGH_TEMPERATURES
+// 只修改这一行上面的代码
