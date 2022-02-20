4e7895c8226c16b86d2f0a365297032d37a3d3e3
richard1230 <yongjinhu1230@gmail.com>
2022-02-20 18:24:18 +0800

    使用展开运算符复制数组

--- Javascript/Js中数组对象常见操作/复制数组元素.js
+++ Javascript/Js中数组对象常见操作/复制数组元素.js
@@ -16,3 +16,34 @@ function forecast(arr) {
 
 // 只修改这一行上面的代码
 console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+
+
+//使用展开运算符复制数组
+// 展开运算符（spread operator），它能让我们方便地复制数组中的所有元素。
+// 展开语法写出来是这样：...
+let thisArray = [true, true, undefined, false, null];
+let thatArray = [...thisArray];
+console.log(thatArray);//[ true, true, undefined, false, null ]
+
+
+function copyMachine(arr, num) {
+  let newArr = [];
+  while (num >= 1) {
+    // 只修改这一行下面的代码
+    newArr.push([...arr])
+    // 只修改这一行上面的代码
+    num--;
+  }
+  return newArr;
+}
+
+console.log(copyMachine([true, false, true], 2));
+/*
+copyMachine([true, false, true], 2) 应返回 [[true, false, true], [true, false, true]]。
+
+copyMachine([1, 2, 3], 5) 应返回 [[1, 2, 3], [1, 2, 3], [1, 2, 3], [1, 2, 3], [1, 2, 3]]。
+
+copyMachine([true, true, null], 1) 应返回 [[true, true, null]]。
+
+copyMachine(["it works"], 3) 应返回 [["it works"], ["it works"], ["it works"]]。
+* */
