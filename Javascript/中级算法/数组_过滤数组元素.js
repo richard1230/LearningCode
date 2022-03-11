8556bc52e06d87f4b7f4abb2d04efc35d48d7127
richard1230 <yongjinhu1230@gmail.com>
2022-03-12 00:48:01 +0800

    过滤数组元素

---
+++ Javascript/中级算法/数组_过滤数组元素.js
@@ -0,0 +1,14 @@
+function destroyer(arr,...needToRemove) {
+  //item是arr里面的元素
+  return arr.filter(item=>needToRemove.indexOf(item)==-1);
+}
+
+
+function destroyer1(arr,...needToRemove) {
+  //item是arr里面的元素
+  return arr.filter(item=>!needToRemove.includes(item));
+}
+
+
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));;
+console.log(destroyer1([1, 2, 3, 1, 2, 3], 2, 3));;
