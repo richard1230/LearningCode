00ac88572d85181c57f7e5fbd9c304db6477ac44
richard1230 <yongjinhu1230@gmail.com>
2022-03-11 23:59:57 +0800

    数组的对称差

---
+++ Javascript/中级算法/数组对称差.js
@@ -0,0 +1,22 @@
+/*
+数组的对称差
+比较两个数组并返回一个新数组，包含所有只在其中一个数组中出现的元素，
+排除两个数组都存在的元素。 换言之，我们需要返回两个数组的对称差。
+**注意：**返回数组中的元素顺序不会影响挑战的测试结果
+* */
+
+function diffArray(arr1, arr2) {
+  const newArr = [];
+  function onlyfirst(one,two) {
+    for (let i = 0; i < two.length; i++) {
+      if (one.indexOf(two[i])== -1){
+        newArr.push(two[i])
+      }
+    }
+  }
+  onlyfirst(arr1,arr2)
+  onlyfirst(arr2,arr1)
+  return newArr;
+}
+
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));;
