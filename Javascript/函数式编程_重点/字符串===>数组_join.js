e6bebbb84a88c2dd459560ab49e13d058a53d4ad
richard1230 <yongjinhu1230@gmail.com>
2022-03-11 22:27:52 +0800

    应用函数式编程将字符串转换为URL片段

--- Javascript/函数式编程_重点/字符串===>数组_join.js
+++ Javascript/函数式编程_重点/字符串===>数组_join.js
@@ -1,8 +1,9 @@
 function sentensify(str) {
   // 只修改这一行下面的代码
 
-return str.split(/\W/).join(" ")
+  return str.split(/\W/).join(" ")
   // 只修改这一行上面的代码
 }
 
-console.log(sentensify("May-the-force-be-with-you"));;
+console.log(sentensify("May-the-force-be-with-you"));
+;
---
+++ Javascript/函数式编程_重点/应用函数式编程将字符串转换为URL片段.js
@@ -0,0 +1,8 @@
+function urlSlug(title) {
+
+ return title.split(/\s+/).filter(str=>str!=="").map(alph=>alph.toLowerCase()).join("-")
+}
+// 只修改这一行上面的代码
+console.log(urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone"));
+
+//urlSlug(" Winter Is  Coming") 应返回 winter-is-coming。
