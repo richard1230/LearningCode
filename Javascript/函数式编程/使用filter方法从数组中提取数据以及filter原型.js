174579e24d1188b3e91864e266a6d23bb4aca626
richard1230 <yongjinhu1230@gmail.com>
2022-03-11 16:12:07 +0800

    使用filter原型

--- Javascript/函数式编程/使用filter方法从数组中提取数据以及filter原型.js
+++ Javascript/函数式编程/使用filter方法从数组中提取数据以及filter原型.js
@@ -136,3 +136,27 @@ const filteredList = watchList.map(user => ({
 // 只修改这一行上面的代码
 
 console.log(filteredList);
+
+
+// =======================
+
+// filter原型
+
+// 全局变量
+const s = [23, 65, 98, 5];
+
+Array.prototype.myFilter = function(callback) {
+  // 只修改这一行下面的代码
+  const newArray = [];
+  // 只修改这一行上面的代码
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i]==true)){
+       newArray.push(this[i])
+    }
+  }
+  return newArray;
+};
+
+const new_s = s.myFilter(function(item) {
+  return item % 2 === 1;
+});
