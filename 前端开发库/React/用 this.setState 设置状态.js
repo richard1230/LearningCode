929082aa507e99faf875c8cb2e5ede04c86094d2
richard1230 <yongjinhu1230@gmail.com>
2022-03-21 21:42:15 +0800

    设置状态

---
+++ 前端开发库/React/用 this.setState 设置状态.js
@@ -0,0 +1,24 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Initial State'
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    // 修改这行下面的代码
+    this.setState({
+      name: 'React Rocks!'
+    });
+    // 修改这行上面的代码
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>Click Me</button>
+        <h1>{this.state.name}</h1>
+      </div>
+    );
+  }
+};
