8ec471828af1e885ac0688df066c8251224f5024
richard1230 <yongjinhu1230@gmail.com>
2022-03-22 16:41:37 +0800

    添加事件侦听

---
+++ 前端开发库/React/34_添加事件侦听器.js
@@ -0,0 +1,35 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: ''
+    };
+    this.handleEnter = this.handleEnter.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+  // 修改这行下面的代码
+  componentDidMount() {
+   document.addEventListener("keydown",this.handleKeyPress)
+  }
+  componentWillUnmount() {
+   document.removeEventListener("keydown",this.handleKeyPress);
+  }
+  // 修改这行上面的代码
+  handleEnter() {
+    this.setState((state) => ({
+      message: state.message + 'You pressed the enter key! '
+    }));
+  }
+  handleKeyPress(event) {
+    if (event.keyCode === 13) {
+      this.handleEnter();
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.message}</h1>
+      </div>
+    );
+  }
+};
