1ca3d99b12cd6cb87eb7d6d2e7ceb40c554ed7ac
richard1230 <yongjinhu1230@gmail.com>
2022-03-22 13:52:03 +0800

    创建一个可以控制的输入框

---
+++ 前端开发库/React/创建一个可以控制的输入框.js
@@ -0,0 +1,31 @@
+class ControlledInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+    // 修改这行下面的代码
+    this.handleChange = this.handleChange.bind(this)
+    // 修改这行上面的代码
+  }
+  // 修改这行下面的代码
+ handleChange(event){
+    this.setState(
+      {
+        input: event.target.value
+      }
+    )
+ }
+  // 修改这行上面的代码
+  render() {
+    return (
+      <div>
+        { /* 修改这行下面的代码 */}
+<input value={this.state.input} onChange={this.handleChange}/>
+        { /* 修改这行上面的代码 */}
+        <h4>Controlled Input:</h4>
+        <p>{this.state.input}</p>
+      </div>
+    );
+  }
+};
