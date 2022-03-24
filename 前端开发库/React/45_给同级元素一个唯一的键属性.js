88bd421d3723d485e079ed26bea5de5ecd09a2e0
richard1230 <yongjinhu1230@gmail.com>
2022-03-24 18:13:21 +0800

    给同级元素一个唯一的键属性

---
+++ 前端开发库/React/45_给同级元素一个唯一的键属性.js
@@ -0,0 +1,20 @@
+const frontEndFrameworks = [
+  'React',
+  'Angular',
+  'Ember',
+  'Knockout',
+  'Backbone',
+  'Vue'
+];
+
+function Frameworks() {
+  const renderFrameworks = frontEndFrameworks.map(item=><li key={item}>{item}</li>); // 修改这一行
+  return (
+    <div>
+      <h1>Popular Front End JavaScript Frameworks</h1>
+      <ul>
+        {renderFrameworks}
+      </ul>
+    </div>
+  );
+};
