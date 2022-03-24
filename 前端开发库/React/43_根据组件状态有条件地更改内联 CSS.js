a23e0b8d814e17d1d04dcdaf9d99e28087035ae0
richard1230 <yongjinhu1230@gmail.com>
2022-03-24 17:55:49 +0800

    根据组件状态有条件地更改内联CSS

---
+++ 前端开发库/React/43_根据组件状态有条件地更改内联 CSS.js
@@ -0,0 +1,34 @@
+class GateKeeper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ input: event.target.value })
+  }
+  render() {
+    let inputStyle = {
+      border: '1px solid black'
+    };
+    // 修改这行下面的代码
+   if(this.state.input.length > 15){
+     inputStyle.border = '3px solid red'
+   }
+    // 修改这行上面的代码
+    return (
+      <div>
+        <h3>Don't Type Too Much:</h3>
+        <input
+          type="text"
+          style={inputStyle}
+          value={this.state.input}
+          onChange={this.handleChange} />
+      </div>
+    );
+  }
+};
+
+//根据组件状态有条件地更改内联CSS
