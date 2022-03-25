f43283ba0443adf610fce72446e5c704fb2fed7d
richard1230 <yongjinhu1230@gmail.com>
2022-03-25 19:34:27 +0800

    发送Action_to_Data

---
+++ 前端开发库/redux/11_发送 Action Data 给 Store.js
@@ -0,0 +1,30 @@
+const ADD_NOTE = 'ADD_NOTE';
+
+const notesReducer = (state = 'Initial State', action) => {
+  switch (action.type) {
+    // 修改这行下面的代码
+    case ADD_NOTE:
+      return action.text;
+    // 修改这行上面的代码
+    default:
+      return state;
+  }
+};
+
+const addNoteText = (note) => {
+  // 修改这行下面的代码
+  return {
+    type: ADD_NOTE,
+    text: note
+  }
+  // 修改这行上面的代码
+};
+
+const store = Redux.createStore(notesReducer);
+
+console.log(store.getState());
+store.dispatch(addNoteText('Hello!'));
+console.log(store.getState());
+
+
+
