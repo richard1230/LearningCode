e0f3bf97e8db2983117a222094babada58a22533
richard1230 <yongjinhu1230@gmail.com>
2022-03-25 22:02:16 +0800

    拷贝对象

---
+++ 前端开发库/redux/17_使用 Object.assign 拷贝对象.js
@@ -0,0 +1,26 @@
+const defaultState = {
+  user: 'CamperBot',
+  status: 'offline',
+  friends: '732,982',
+  community: 'freeCodeCamp'
+};
+
+const immutableReducer = (state = defaultState, action) => {
+  switch(action.type) {
+    case 'ONLINE':
+      // 这里不能修改 state，否则测试不能通过
+      let a = Object.assign({},state);
+      a.status = 'online';
+      return a;
+    default:
+      return state;
+  }
+};
+
+const wakeUp = () => {
+  return {
+    type: 'ONLINE'
+  }
+};
+
+const store = Redux.createStore(immutableReducer);
