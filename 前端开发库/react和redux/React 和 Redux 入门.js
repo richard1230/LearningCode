07257e6efab72ab1521f39f79b691d5344e0230a
richard1230 <yongjinhu1230@gmail.com>
2022-03-26 20:04:54 +0800

    react_redux入门

---
+++ 前端开发库/react和redux/React 和 Redux 入门.js
@@ -0,0 +1,14 @@
+class DisplayMessages extends React.Component {
+  // 修改这行下面的代码
+  constructor(props) {
+    super(props);
+    this.state={
+      input:'',
+      message:[]
+    }
+  }
+  // 修改这行上面的代码
+  render() {
+    return <div />
+  }
+};
