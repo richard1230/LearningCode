b06587c24b95bb1b937e8c2307eb3eb8b0624689
richard1230 <yongjinhu1230@gmail.com>
2022-03-29 17:34:25 +0800

    预编译总结add

--- Javascript/JavaScript基本功/函数相关/test1.js
+++ Javascript/JavaScript基本功/函数相关/test1.js
@@ -8,13 +8,14 @@ function test1(e) {
 
   arguments[0] = 2;
   console.log(e);//2
+  a = 4;
   if (a) {
     var b = 3;
   }
   var c;
-  a = 4;
+  // a = 4;
   var a;
-  console.log(b);//undefined
+  console.log(b);//3
   f = 5;
   console.log(c); //undefined
   console.log(a);//4
@@ -40,7 +41,7 @@ AO={
  function e(){} ->
  2,
 
- b:undefined,
+ b:undefined ---> 3
  c:undefined,
  a:undefined -> 4
  }
