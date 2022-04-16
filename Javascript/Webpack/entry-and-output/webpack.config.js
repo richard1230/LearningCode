bc11483c6529905afd087b60636bca50362d89d6
richard1230 <yongjinhu1230@gmail.com>
2022-04-16 15:40:09 +0800

    Webpack01-改变出口文件名字

--- Javascript/Webpack/entry-and-output/webpack.config.js
+++ Javascript/Webpack/entry-and-output/webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry:{
+    test: './src/index.js'
+  } ,
   output:{
     path: path.resolve(__dirname,'dist'),
-    filename: 'main.js'
+    // filename: 'main.js'
   }
 }
