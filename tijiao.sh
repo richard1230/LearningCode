2baa67ace5f3aee14d920ead8191e9e5d835a407
richard Hu <yongjinhu1230@gmail.com>
2022-08-29 19:03:18 +0800

    0828-tijiao-sh

--- tijiao.sh
+++ tijiao.sh
@@ -1,7 +1,10 @@
 #！/bin/bash
 
+unset http_proxy  &&
+unset https_proxy  &&
 git add . &&
 git commit -m "$1" &&
 git pull  &&
-git push -u origin main
-#git push -u gitlab-codelearning main
+git push -u origin main &&
+echo "hello this is github flow================then gitlab, please" &&
+git push -u gitlab-codelearning main
