584da768929dfddc03a19b2a05b9ce9f97ee4f21
richard Hu <yongjinhu1230@gmail.com>
2022-08-30 18:31:33 +0800

    0830-css-没有边框的盒子

--- tijiao.sh
+++ tijiao.sh
@@ -6,7 +6,7 @@ git add . &&
 git commit -m "$1" &&
 git pull  &&
 git push -u origin main &&
-echo "hello this is github flow================then gitlab, please" &&
+echo "hello this is github flow==================================================================then gitlab, please" &&
 git push -u gitlab-codelearning main
 
 
