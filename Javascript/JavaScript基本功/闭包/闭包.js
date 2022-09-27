9a5da0735b31605fc06aa23299e56807175821c2
richard Hu <yongjinhu1230@gmail.com>
2022-09-27 10:05:28 +0800

    立即执行函数 0927-2022

--- Javascript/JavaScript基本功/闭包/闭包.js
+++ Javascript/JavaScript基本功/闭包/闭包.js
@@ -56,59 +56,59 @@ test1(fn1)//b: 2
 console.log("闭包测试: ==============")
 
 //累加器
-// function test5() {
-//   var num = 0;
-//   function add() {
-//     console.log(++num)
-//     // return function () {
-//     //   console.log(++num)
-//     // }
-//   }
-//   return add;
-// }
-// var add = test5();
-// console.log(add());
-// console.log(add());
-// console.log(add());
-// console.log(add());
-// console.log(add());
-//
+function test5() {
+  var num = 0;
+  function add() {
+    console.log(++num)
+    // return function () {
+    //   console.log(++num)
+    // }
+  }
+  return add;
+}
+var add = test5();
+console.log(add());//1 undefined
+console.log(add());//2 undefined
+console.log(add());//3 undefined
+console.log(add());//4 undefined
+console.log(add());//5 undefined
+
 
 console.log("闭包测试...>>>: ==============")
 
-const testClosure = () => {
-  let num = 0;
-  const effect = () => {
-    num += 1;
-    const message = `num value in message：${num}`;
-    return function unmount() {
-      console.log("这是在effect里面的unmount: ",message);
-    };
-    // console.log(num)
-  };
-  return effect;
-};
-// 执行test，返回effect函数
-const add = testClosure();
-// 执行effect函数，返回引用了message1的unmount函数
-var unmount = add();
-// 再一次执行effect函数，返回引用了message2的unmount函数
-console.log(add);;//[Function: effect]
-// message3
-var unmount01 = add()
-console.log(unmount01);;//[Function: unmount]
-
-console.log(unmount01 === unmount)//false
-
-// message4
-// console.log(add());;
-// // message5
-// console.log(add());;
-unmount();// 在这里会打印什么呢？按照直觉似乎应该打印5,实际上打印了1
-
-unmount()
-
-unmount()
+// const testClosure = () => {
+//   let num = 0;
+//   const effect = () => {
+//     num += 1;
+//     const message = `num value in message：${num}`;
+//     return function unmount() {
+//       console.log("这是在effect里面的unmount: ",message);
+//     };
+//     // console.log(num)
+//   };
+//   return effect;
+// };
+// // 执行test，返回effect函数
+// const add = testClosure();
+// // 执行effect函数，返回引用了message1的unmount函数
+// var unmount = add();
+// // 再一次执行effect函数，返回引用了message2的unmount函数
+// console.log(add);;//[Function: effect]
+// // message3
+// var unmount01 = add()
+// console.log(unmount01);;//[Function: unmount]
+//
+// console.log(unmount01 === unmount)//false
+//
+// // message4
+// // console.log(add());;
+// // // message5
+// // console.log(add());;
+// unmount();// 在这里会打印什么呢？按照直觉似乎应该打印5,实际上打印了1
+//
+// unmount()
+//
+// unmount()
 
 
 
