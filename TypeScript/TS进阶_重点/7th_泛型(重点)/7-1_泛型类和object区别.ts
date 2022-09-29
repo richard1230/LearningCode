d9ec89c236fc055201821ae4ddac1d3dbd673788
richard Hu <yongjinhu1230@gmail.com>
2022-09-29 16:15:04 +0800

    泛型

---
+++ TypeScript/TS进阶_重点/7th_泛型(重点)/7-1_泛型类和object区别.ts
@@ -0,0 +1,15 @@
+
+
+
+// let obj : object =3
+
+let obj:Object = 3
+let obj1:Object = "string"
+let obj2:Object = undefined
+let obj3:Object = null
+let obj4:Object = {name:"Lily"}
+let obj5:Object = [1,2]
+let obj6:Object = function () {
+  console.log(1);
+}
+
